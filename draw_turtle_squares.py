f3117bf540473d4a101efd30a588b84960246551
Lyn Stanford <lynstanford@outlook.com>
2020-05-12 23:46:26 -0400

    Create draw_turtle_squares.py
    Draws a series of squares and rotates 6 degrees each time before repeating until a whole circle has been drawn

---
+++ draw_turtle_squares.py
@@ -0,0 +1,28 @@
+from turtle import *
+import turtle
+
+# First you need to define a loop function to draw a square
+
+def square():
+    for side in range(4):  # unused variable
+        turtle.forward(150)
+        turtle.right(90)
+
+
+# Ask the user for input if they wish to see the Turtle move
+question = input("Do you wish to see my animation? y/n: ")
+answer = question.lower() in ('y', 'yes')
+
+turtle.shape('turtle')
+turtle.speed('fastest')
+
+if answer:
+    turtle.bgcolor('turquoise')
+    turtle.color('white')
+
+    for repetition in range(60):  # unused variable
+        square()
+        turtle.right(6)
+
+turtle.hideturtle()
+turtle.done()
